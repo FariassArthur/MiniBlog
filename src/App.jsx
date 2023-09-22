de7a8c6d24bb9771da340668914c09cf1487104d
arthurfarias-debian <arthurfarias-debian@debian.arthurfarias-debian>
2023-09-22 19:01:49 -0300

    User att

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { onAuthStateChanged } from 'firebase/auth';
 //hooks
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useAuthentication } from './hooks/useAuthentication';
+import useAuthentication from './hooks/useAuthentication';
  
 //pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 //components
 import Navbar from './components/Navbar';
@@ -30,7 +32,7 @@ function App() {
   useEffect(() => {
 
     onAuthStateChanged(auth, (user) => {
-      setUser(user)
+      setUser(user);
     });
 
   }, [auth]);
@@ -48,8 +50,11 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
+              <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
+              {}
             </Routes>
           </div>
           <Footer />
--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -33,6 +33,7 @@ const useAuthentication = () => {
         }
     } //evita vazamento de memória
 
+    //register
     const createUser = async (data) => {
         checkIfIsCancelled()
 
@@ -74,6 +75,51 @@ const useAuthentication = () => {
         }
     };
 
+    //logout - sign out
+    const logout = () => {
+        checkIfIsCancelled();
+
+        signOut(auth)
+
+    };
+
+    //login - sign in
+    const login = async(data) => {
+
+        checkIfIsCancelled();
+
+        setLoading(true)
+        setError(false)
+
+        try {
+
+            await signInWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password,
+            )
+
+            setLoading(false)
+
+
+        } catch (error) {
+
+            let systemErrorMessage;
+
+            if (error.message.includes("user-not-found")) {
+                systemErrorMessage = "Usuário não encontrado."
+            } else if(error.message.includes("wrong-password")) {
+                systemErrorMessage = "Senha incorreta."
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde"
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
+
+    }
+
     useEffect(() => {
         return () => setCancelled(true);
     }, [])
@@ -85,6 +131,8 @@ const useAuthentication = () => {
         createUser,
         error,
         loading,
+        logout,
+        login,
     };
 }
 
